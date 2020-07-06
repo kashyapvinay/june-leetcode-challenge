ea14fcfa35d1e00d0d30caf18192cf2565bf057d
Vinay Kashyap <vinaykashyap60@gmail.com>
2020-07-06 14:05:51 +0530

    Create day_6_Plus One.py

---
+++ python3/july/day_6_Plus One.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        c = 1
+        for i in range(len(digits)-1, -1, -1):
+            s = digits[i] + c
+            c = s // 10
+            digits[i] = s % 10
+            if c == 0:
+                break
+        if(c):
+            digits.insert(0, c)
+
+        return digits
